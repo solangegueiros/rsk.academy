01972e2de7929679d3541f0232427d99622060e3
7alip <talipaltas@gmail.com>
2021-03-18 20:43:38 +0100

    Remove: blog component

--- src/utils/mdxUtils/index.js
+++ src/utils/mdxUtils/index.js
@@ -1,6 +1,4 @@
 const path = require('path')
-const fromUnixTime = require('date-fns/fromUnixTime')
-const format = require('date-fns/format')
 const { getEditUrl, addLeadingSlash } = require('@docusaurus/utils')
 
 function fileToPath(str) {
@@ -8,7 +6,7 @@ function fileToPath(str) {
 }
 
 function processFrontmatter(options) {
-  const { path: mdxPath, author, tags, baseEditUrl, ...rest } = options
+  const { path: mdxPath, tags, baseEditUrl, ...rest } = options
 
   const editUrl = getEditUrl(path.join(mdxPath), baseEditUrl)
 
@@ -23,29 +21,7 @@ function processFrontmatter(options) {
   }
 }
 
-/**
- * Format the last edited timestamp and author from git output
- */
-function getTimestampAndAuthor(str) {
-  if (!str) return null
-
-  const GIT_COMMIT_TIMESTAMP_AUTHOR_REGEX = /^(\d+), (.+)$/
-  const temp = str.match(GIT_COMMIT_TIMESTAMP_AUTHOR_REGEX)
-
-  if (!temp || temp.length < 3) return null
-
-  // eslint-disable-next-line no-unused-vars
-  const [_, timestamp, author] = temp
-  const dateStr = fromUnixTime(+timestamp)
-
-  return {
-    date: format(dateStr, 'MMMM dd, yyyy'),
-    author,
-  }
-}
-
 module.exports = {
-  getTimestampAndAuthor,
   fileToPath,
   processFrontmatter,
 }
