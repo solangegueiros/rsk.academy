425a3c9b96c10b4e64e64631d6bf5722e5814616
Talip Altas <talipaltas@gmail.com>
2021-04-27 14:25:56 +0200

    create: course list in profile page (#86)

--- src/pages/profile.js
+++ src/pages/profile.js
@@ -1,16 +1,24 @@
 import { useContext } from 'react'
 import { useSelector } from 'react-redux'
-import { Box, Heading, useColorModeValue } from '@chakra-ui/react'
+import {
+  Alert,
+  AlertIcon,
+  Box,
+  Heading,
+  SimpleGrid,
+  useColorModeValue,
+} from '@chakra-ui/react'
 import { useI18n } from 'next-localization'
 
-import { Layout, Seo } from '@/components/all'
+import { Layout, Seo, SubscribeAcademy } from '@/components/all'
 import { RLoginResponseContext } from '@/context/RLoginProvider'
+import { COURSE_ADDRESSES } from '@/constants/constants'
 
 const Profile = () => {
   const { t } = useI18n()
   const profile = useSelector(state => state.profile)
   const { rLoginResponse } = useContext(RLoginResponseContext)
-  const { isAdmin } = useSelector(state => state.identity)
+  const { isAdmin, chainId } = useSelector(state => state.identity)
   const {
     index,
     ownerAddress,
@@ -24,73 +32,95 @@ const Profile = () => {
 
   const color = useColorModeValue('primary.500', 'light.500')
 
+  const renderProfile = () => {
+    if (!rLoginResponse) return <Box>You must log in</Box>
+    if (isAdmin) return <Box>Portfolio page is for students</Box>
+    if (!index)
+      return (
+        <Box>
+          <Alert status='warning' mb={8}>
+            <AlertIcon />
+            You are not registered in RSK Academy. Please subscribe one of the
+            following courses.
+          </Alert>
+          <SimpleGrid columns={{ base: 1, md: 2 }} spacing={4}>
+            {Object.keys(COURSE_ADDRESSES[chainId]).map(courseName => (
+              <SubscribeAcademy key={courseName} contractName={courseName} />
+            ))}
+          </SimpleGrid>
+        </Box>
+      )
+
+    return (
+      <>
+        <Box my={8} p={4} boxShadow='md' as='pre'>
+          <Heading>{studentName}</Heading>
+          {ownerAddress && (
+            <Box my={4}>
+              <Box fontWeight='bold' color={color}>
+                Owner Address:
+              </Box>
+              <pre>{ownerAddress}</pre>
+            </Box>
+          )}
+
+          {portfolioAddress && (
+            <Box my={4}>
+              <Box fontWeight='bold' color={color}>
+                Portfolio Address:
+              </Box>
+              <pre>{portfolioAddress}</pre>
+            </Box>
+          )}
+
+          {portfolioList && (
+            <Box my={4}>
+              <Box fontWeight='bold' color={color}>
+                Projects in portfolio:
+              </Box>
+              {portfolioList.map(([address, name]) => (
+                <pre key={address}>
+                  {name} - {address}
+                </pre>
+              ))}
+            </Box>
+          )}
+          {studentClasses && (
+            <Box my={4}>
+              <Box fontWeight='bold' color={color}>
+                Classes subscribed:
+              </Box>
+              {studentClasses.map(c => (
+                <pre key={c}>{c}</pre>
+              ))}
+            </Box>
+          )}
+
+          {studentActiveClassName && (
+            <Box my={4}>
+              <Box fontWeight='bold' color={color}>
+                Active Class:
+              </Box>
+              <pre>
+                {studentActiveClassName} - {activeClassAddress}
+              </pre>
+            </Box>
+          )}
+        </Box>
+        <Heading mb={8}>Courses</Heading>
+        <SimpleGrid columns={{ base: 1, md: 2 }} spacing={8}>
+          {Object.keys(COURSE_ADDRESSES[chainId]).map(courseName => (
+            <SubscribeAcademy key={courseName} contractName={courseName} />
+          ))}
+        </SimpleGrid>
+      </>
+    )
+  }
+
   return (
     <Layout>
       <Seo title={t('profile')} />
-      {rLoginResponse ? (
-        isAdmin ? (
-          <Box>Portfolio page is for students</Box>
-        ) : index ? (
-          <Box my={4} p={4} boxShadow='md' as='pre'>
-            <Heading>{studentName}</Heading>
-            {ownerAddress && (
-              <Box my={4}>
-                <Box fontWeight='bold' color={color}>
-                  Owner Address:
-                </Box>
-                <pre>{ownerAddress}</pre>
-              </Box>
-            )}
-
-            {portfolioAddress && (
-              <Box my={4}>
-                <Box fontWeight='bold' color={color}>
-                  Portfolio Address:
-                </Box>
-                <pre>{portfolioAddress}</pre>
-              </Box>
-            )}
-
-            {portfolioList && (
-              <Box my={4}>
-                <Box fontWeight='bold' color={color}>
-                  Projects in portfolio:
-                </Box>
-                {portfolioList.map(([address, name]) => (
-                  <pre key={address}>
-                    {name} - {address}
-                  </pre>
-                ))}
-              </Box>
-            )}
-            {studentClasses && (
-              <Box my={4}>
-                <Box fontWeight='bold' color={color}>
-                  Classes subscribed:
-                </Box>
-                {studentClasses.map(c => (
-                  <pre key={c}>{c}</pre>
-                ))}
-              </Box>
-            )}
-
-            {studentActiveClassName && (
-              <Box my={4}>
-                <Box fontWeight='bold' color={color}>
-                  Active Class:
-                </Box>
-                <pre>
-                  {studentActiveClassName} - {activeClassAddress}
-                </pre>
-              </Box>
-            )}
-          </Box>
-        ) : (
-          <Box>Student is not registered in RSK Academy.</Box>
-        )
-      ) : (
-        <Box>You must log in</Box>
-      )}
+      {renderProfile()}
     </Layout>
   )
 }
