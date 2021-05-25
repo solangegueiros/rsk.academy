d38f5db85341ca133e1f7c6872ea6de65f464970
Talip Altas <talipaltas@gmail.com>
2021-05-25 13:58:30 +0200

    fix: solidity theme & mdx page container (#101)

--- pages/api/events/index.ts
+++ pages/api/events/index.ts
@@ -1,6 +1,7 @@
 import { getEvents } from '@lib/getEvents'
+import { NextApiRequest, NextApiResponse } from 'next'
 
-export default async function handler(req, res): Promise<void> {
+export default async function handler(req: NextApiRequest, res: NextApiResponse): Promise<void> {
   const { id } = req.query
   try {
     const response = await getEvents()
--- src/utils/slash/index.ts
+++ src/utils/slash/index.ts
@@ -1,4 +1,4 @@
-export const slash = path => {
+export const slash = (path: string): string => {
   const isExtendedLengthPath = /^\\\\\?\\/.test(path)
   const hasNonAscii = /[^\u0000-\u0080]+/.test(path) // eslint-disable-line no-control-regex
 
